cefa06777a754d7abf7268897da33476fe96c815
Prateek Priyadarshi <prateekpriyadarshi1328@gmail.com>
2022-09-14 01:54:19 +0530

    User Login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,12 @@ const asyncHandler = require("express-async-handler");
 
 const User = require("../model/userModel");
 
+
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+
+
+
 const loginUser = (req, res) => {
 
     res.json({message:"login user"})
@@ -11,26 +17,43 @@ const loginUser = (req, res) => {
 
 const registerUser = asyncHandler(async (req, res) => {
     console.log("registerUser req.body=====>", req.body);
+    const {email, password, name} = req.body
   
-    if (!req.body.name) {
+    if (!name) {
       res.status(400);
       throw new Error("Please Add A Name");
     }
-    if (!req.body.email) {
+    if (!email) {
         res.status(400);
         throw new Error("Please Add An Email");
       }
-      if (!req.body.password) {
+      if (!password) {
         res.status(400);
         throw new Error("Please Add A Password");
       }
       
 
-  
-    const user = await User.create({ name: req.body.name, email: req.body.email, password: req.body.password});
-  
-     res.json(user);
+    const userExists = await User.findOne({ email: email})
+    if(userExists) {
+      res.status(400);
+      throw new Error("User Already Exists")
+    }  
 
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const user = await User.create({ name: name, email: email, password:hashedPassword});
+      
+   if(user) {
+    res.status(201).json({ 
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    })
+   }else{
+    res.status(400)
+    throw new Error("Invalid User Data")
+   }
 
 
 
